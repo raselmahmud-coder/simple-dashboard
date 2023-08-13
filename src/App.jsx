0049f0be7697a46a4d66025d3a89a399bbde2bed
raselmahmud22 <raselmahmud454b@gmail.com>
2023-08-13 20:25:21 +0600

    project init

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <h1 className="text-3xl bg-blue font-bold underline text-yellow">
+      Hello world!
+    </h1>
+  )
+}
+
+export default App
